0536a718e55aa94025fbd28b8d708df46712105f
Innokenty Sapog <ghjpa@inbox.ru>
2023-08-22 00:08:10 +0300

    refactor

--- src/main/java/com/moonlightmoth/housedevil/testping/Application.java
+++ src/main/java/com/moonlightmoth/housedevil/testping/Application.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.jar.Manifest;
+
 @SpringBootApplication
 @RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		System.out.println("Starting ping service of version 1.0");
 	}
 
 	@GetMapping(path = "/ping")
